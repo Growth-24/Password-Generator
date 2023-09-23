602b495c1a24f4774675363c4bd992152d728871
WMJ <waynemyriejr@gmail.com>
2023-09-23 09:57:25 -0400

    Password Generator Project

---
+++ co8384fdd8ee047b529fdcd95/index.js
@@ -0,0 +1,33 @@
+const characters =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+const btnText = document.getElementById("btn-text")
+const passwordOneEl = document.getElementById("text-1")
+const passwordTwoEl = document.getElementById("text-2")
+let passwordLength = 15
+
+
+function getRandomCharacter() {
+    const randomChar = Math.floor(Math.random() * characters.length)
+    return characters[randomChar]
+}
+
+function generateRandomPassword() {
+    let randomPassword = ""
+    for (let i = 0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter() 
+              
+    }
+    return randomPassword
+}
+
+
+
+function btnclick() {
+    const generatedPasswordOne = generateRandomPassword()
+    const generatedPasswordTwo = generateRandomPassword()
+    passwordOneEl.textContent = generatedPasswordOne
+    passwordTwoEl.textContent = generatedPasswordTwo 
+   
+}
+
